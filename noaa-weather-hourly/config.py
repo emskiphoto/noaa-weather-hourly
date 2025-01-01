bc4bb8768fc83f4bf882a753cf93ab93105f4d19
Matt Chmielewski <43832628+emskiphoto@users.noreply.github.com>
2024-12-31 18:25:26 -0600

    dev automatic version and file(s) selection

--- noaa-weather-hourly/config.py
+++ noaa-weather-hourly/config.py
@@ -1,4 +1,15 @@
 # config.py
+# noaa-weather-hourly
+freqstr = 'H'
+# v1 LCD csv file name regex
+pattern_lcd1_input_file = r'^[0-9]{5,10}.csv'
+# v2 LCD csv file name regex
+pattern_lcd2_input_file = r'^LCD_(.*)_[0-9]{4}.csv'
+patterns_lcd_input_files = [pattern_lcd1_input_file, pattern_lcd2_input_file]
+version_pattern_lcd_input = {idx_ : v_ for idx_, v_ in enumerate(patterns_lcd_input_files, start=1)}
+pattern_isd_history_file = r'isd-history.csv|ISD-HISTORY.CSV'
+file_output_format = """{STATION_NAME} {from_str} to {end_str} {freqstr}.csv"""
+pct_null_timestamp_max = 0.5
 
 freqstr_frequency = {'D': 'Daily',
 'W': 'Weekly',
--- noaa-weather-hourly/utils.py
+++ noaa-weather-hourly/utils.py
@@ -1,6 +1,43 @@
 # utils.py
-
+# noaa-weather-hourly
+import pathlib
+import re
 
 def say_hello():
     print('Hello from the Utils.py module')
     
+def find_latest_file(directory, pattern):
+    """Returns pathlib file path object for most recently modified file in 'directory' whose
+    path/name matches 'pattern'.  The 'pattern' is the same pattern that would be input in the
+    pathlib.glob() function.  
+    Example Usage:  path = find_latest_file(pathlib.Path('data'), '*Minnesota.csv')"""
+    try:
+        return sorted([(f, f.stat().st_mtime) for f in directory.glob('*') 
+                       if re.match(pattern, f.name)],
+           key=lambda x: x[1], reverse=True)[0][0]
+    except:
+        return None
+    
+def find_file_re_pattern(directory, pattern):
+    """Returns list of file names (name only, not paths) in the
+    input Pathlib 'directory' that match
+    the regular expression 'pattern' provided"""
+    try:
+        return [x for x in directory.glob('*') if
+                          re.search(pattern, x.name) != None]
+    except:
+        return None
+    
+    
+def find_files_re_pattern_sorted_last_modified(directory, pattern, descending=True):
+    """Returns list of pathlib file path objects for most recently modified file
+    in 'directory' whose path/name matches 'pattern'.  The 'pattern' is the same pattern
+    that would be input in the pathlib.glob() function.  
+    Example Usage:  paths = find_file_re_pattern_sorted_last_modified(
+                    pathlib.Path('data'), '*Minnesota.csv')"""
+    try:
+        return [x_[0] for x_ in sorted([(f, f.stat().st_mtime) for f in 
+                         find_file_re_pattern(directory, pattern)],
+                           key=lambda x: x[1], reverse=descending)]
+    except:
+        return None
