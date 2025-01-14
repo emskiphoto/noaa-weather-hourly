e2f2e466ab2dc6d613ba550bf894c08c3b97550d
Matt Chmielewski <43832628+emskiphoto@users.noreply.github.com>
2025-01-14 11:37:06 -0600

    updated version to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name = 'noaa_weather_hourly',
-    version = '0.1.0',
+    version = '1.0.1',
     packages = ['noaa_weather_hourly'],
     entry_points = {
         'console_scripts': [
